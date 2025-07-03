c3e0003012bc335a850e4360536877c307021568
Natalya Pikaleva <deva032006@yandex.ru>
2025-07-03 16:49:50 +0300

    Update profile

---
+++ frontend/src/components/Profile_components/WishListProfile.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface WishListProps {
+  wishes: string[]; // или более сложная структура
+}
+
+const WishList: React.FC<WishListProps> = ({ wishes }) => {
+  return (
+    <div>
+      <h3 className="text-2xl font-semibold mb-4">Мои желания</h3>
+      <ul>
+        {wishes.map((wish, index) => (
+          <li key={index} className="mb-2">{wish}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default WishListProfile;
